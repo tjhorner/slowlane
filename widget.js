8a0803b4e5d8c57faf1c280d0b1c969c36bd1c65
TJ Horner <me@tjhorner.com>
2017-07-09 02:20:45 -0700

    add date checking, already seen check

--- widget.js
+++ widget.js
@@ -1,18 +1,28 @@
-var iframe = document.createElement("iframe")
+var jul12 = new Date(2017, 6, 12)
+var today = new Date()
 
-iframe.src = "https://tjhorner.com/slowlane/"
-iframe.id = "slowlane-notice"
-iframe.frameBorder = false
-iframe.style.width = "100%"
-iframe.style.height = "100%"
-iframe.style.position = "fixed"
-iframe.style.top = "0"
-iframe.style.left = "0"
-iframe.style.background = "white"
-iframe.style.zIndex = 9999
+var seen = document.cookie.split(";").map(cookie => cookie.split("=")).filter(cookie => cookie[0].trim() === "slowlane_seen")[0]
 
-window.addEventListener("message", e => {
-  if(e.data === "slowlane-close") document.getElementById("slowlane-notice").style.display = "none"
-})
+if((!seen && today.getFullYear() === jul12.getFullYear() &&
+             today.getMonth() === jul12.getMonth() &&
+             today.getDate() === jul12.getDate()) || location.hash.indexOf("slowlanedebug") !== -1){
+  var iframe = document.createElement("iframe")
 
-document.body.appendChild(iframe)
+  iframe.src = "https://tjhorner.com/slowlane/"
+  iframe.id = "slowlane-notice"
+  iframe.frameBorder = false
+  iframe.style.width = "100%"
+  iframe.style.height = "100%"
+  iframe.style.position = "fixed"
+  iframe.style.top = "0"
+  iframe.style.left = "0"
+  iframe.style.background = "white"
+  iframe.style.zIndex = 9999
+
+  window.addEventListener("message", e => {
+    if(e.data === "slowlane-close") document.getElementById("slowlane-notice").style.display = "none"
+    document.cookie = "slowlane_seen=1"
+  })
+
+  document.body.appendChild(iframe)
+}
